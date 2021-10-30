444b36aec02f8d1386a3e93c37a9f1c71a0db4c3
BJR Matos <bjrmatos@gmail.com>
2021-10-30 15:59:20 -0500

    add implementation

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,59 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--borderRadius': 4 + 'px',
+    '--height': 8 + 'px',
+    '--padding': 0 + 'px'
+  },
+  medium: {
+    '--borderRadius': 4 + 'px',
+    '--height': 12 + 'px',
+    '--padding': 0 + 'px'
+  },
+  large: {
+    '--borderRadius': 8 + 'px',
+    '--height': 24 + 'px',
+    '--padding': 4 + 'px'
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      ariva-valuemax={100}
+      value={value}
+      style={styles}
+    >
+      <VisuallyHidden>
+        {value}%
+      </VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--borderRadius);
+
+  &:before {
+    content: '';
+    display: block;
+    background-color: ${COLORS.primary};
+    width: ${props => props.value}%;
+    height: 100%;
+    border-radius: 4px ${props => props.value >= 99 ? '4px 4px' : '0 0'} 4px;
+  }
+`
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,12 +7,63 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const iconSize = 24
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <RealSelect value={value} onChange={onChange}>
+        {children}
+      </RealSelect>
+      <FancySelect>
+        {displayedValue}
+        <IconWrapper style={{ '--size': iconSize + 'px' }}>
+          <Icon id="chevron-down" size={iconSize} color={COLORS.GRAY_DARK} />
+        </IconWrapper>
+      </FancySelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const RealSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const FancySelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  font-size: ${16/16}rem;
+
+  ${RealSelect}:focus + & {
+    outline: 1px dotted hsl(212, 100%, 40%);
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+
+  ${RealSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`
 
 export default Select;
